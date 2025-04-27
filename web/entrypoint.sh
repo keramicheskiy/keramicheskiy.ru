e8e888c613dc80a78887da3d65eed70acd4a8085
keramicheskiy <Sweetie.77@mail.ru>
2025-04-28 02:09:37 +0300

    make simple authentication

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -6,6 +6,4 @@ python manage.py collectstatic --noinput
 # Копирование новых статические файлы из /web/static в volume static-data
 cp -r /web/static/* /static-data/
 
-celery --app=web worker --loglevel=info
-
 exec gunicorn web.wsgi:application --bind 0.0.0.0:8000
