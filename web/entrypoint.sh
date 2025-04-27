4f84bdbef6e8d5ce1d1c34f0f7efc51755528167
keramicheskiy <Sweetie.77@mail.ru>
2025-04-27 13:40:45 +0300

    do something

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# Выполняем collectstatic
-echo "Собираем статические файлы..."
+
 python manage.py collectstatic --noinput
 
 # Копируем новые статические файлы из /web/static в volume static-data
-echo "Перезаписываем файлы в volume static-data..."
 cp -r /web/static/* /static-data/
 
 # Теперь запускаем основной процесс (например, gunicorn)
